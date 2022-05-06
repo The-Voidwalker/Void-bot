a335911ee7e69c53034f8db963fdd71ed2769363
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2022-05-05 21:25:52 -0400

    mirahezebots renamed to fossbots

--- voidbot.py
+++ voidbot.py
@@ -46,7 +46,7 @@ class VoidBot(SingleServerIRCBot):
             'meta': Api('miraheze', 'meta.miraheze.org'),
             'cvt': Api('miraheze', 'cvt.miraheze.org'),
             'testadminwiki': Api('testadminwiki', 'testwiki.wiki', script_path=''),
-            'botwiki': Api('miraheze', 'wiki.mirahezebots.org')
+            'botwiki': Api('miraheze', 'wiki.fossbots.org')
         }
         self.probably_connected = True
         self.reactor.scheduler.execute_every(600, self.check_connection)
